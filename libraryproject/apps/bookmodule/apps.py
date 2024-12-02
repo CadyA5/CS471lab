b40e231df9c78c6acdd2ec9c401a49c2570199b6
CadyA5 <412205500@qu.edu.sa>
2024-12-02 21:03:12 +0300

    Add populate_books.py for Lab 7

--- libraryproject/apps/bookmodule/apps.py
+++ libraryproject/apps/bookmodule/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from django.shortcuts import render
 
-
-class BookmoduleConfig(AppConfig):
+class UsermoduleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookmodule'
+    name = 'apps.bookmodule'
+
+def links(request):
+    return render(request, 'bookmodule/links.html')
+
+def formatting(request):
+    return render(request, 'bookmodule/formatting.html')
+
+def listing(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables(request):
+    return render(request, 'bookmodule/tables.html')
--- libraryproject/apps/bookmodule/views.py
+++ libraryproject/apps/bookmodule/views.py
@@ -1,3 +1,55 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    name = request.GET.get("name") or "world!"
+    return render(request, "bookmodule/index.html", {"name": name})
+
+def index2(request, val1=0):
+    return HttpResponse("value1 = " + str(val1))
+
+def viewbook(request, bookId):
+    book1 = {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
+    book2 = {'id': 456, 'title': 'Secrets of Reverse Engineering', 'author': 'E. Eilam'}
+    targetBook = None
+    if book1['id'] == bookId:
+        targetBook = book1
+    elif book2['id'] == bookId:
+        targetBook = book2
+    context = {'book': targetBook}
+    return render(request, 'bookmodule/show.html', context)
+
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
+
+def __getBooksList():
+    book1 = {'id': 12344321, 'title': 'Continuous Delivery', 'author': 'J.Humble and D. Farley'}
+    book2 = {'id': 56788765, 'title': 'Reversing: Secrets of Reverse Engineering', 'author': 'E. Eilam'}
+    book3 = {'id': 43211234, 'title': 'The Hundred-Page Machine Learning Book', 'author': 'Andriy Burkov'}
+    return [book1, book2, book3]
+
+def search(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        
+        books = __getBooksList()
+        newBooks = []
+        
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower():
+                contained = True
+            if not contained and isAuthor and string in item['author'].lower():
+                contained = True
+            
+            if contained:
+                newBooks.append(item)
+        
+        return render(request, 'bookmodule/bookList.html', {'books': newBooks})
+    
+    return render(request, 'bookmodule/search.html')
